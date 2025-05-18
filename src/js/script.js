5557b7a4f4f20a0f64680fabae785981f5c493ef
ingridlemoscaetano <ingridlc42@gmail.com>
2025-05-17 22:16:22 -0300

    style: create button and menu toggle

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
-// alert('Conectado!')
+let botao = document.getElementById('menu-toggle')
+let menu_mobile = document.getElementById('menu-mobile')
+
+botao.addEventListener('click', function(){
+    botao.classList.toggle('active')
+    menu_mobile.classList.toggle('show')
+})
+
+// let botao = document.getElementById('menu-toggle')
+// let menuMobile = document.getElementById('menu-mobile')
+
+
+// botao.addEventListener('click',function(){
+//     menuMobile.classList.toggle('show')
+//     botao.classList.toggle('active')
+// })
